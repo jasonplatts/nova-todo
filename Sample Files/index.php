cc3181f05cdd98c5a0c4060140d82e83d9c14ced
Jason Platts <plattsjm@icloud.com>
2020-10-21 17:39:42 -0400

    Added sample files.

---
+++ Sample Files/index.php
@@ -0,0 +1,9 @@
+<?php
+  echo "This is a sample PHP file.";
+  
+  echo "There is still something to do here.";
+  /* TODO: Must do this thing. TODO: This needs to be fixed. */ num = 1; //FIXME: Bad practice.
+  
+  echo "An error should occur here.";
+  //FIXME: Fix this error with the code.
+?>
