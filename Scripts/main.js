4e35f415e32cfb24878e54f38e4861a2369fb747
Jason Platts <plattsjm@icloud.com>
2020-10-29 18:59:58 -0400

    Updated indenting for consistency to 2 spaces :D

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,56 +1,54 @@
-
 const { ToDoDataProvider } = require("./ToDoDataProvider.js");
 
 var treeView = null;
 
 exports.activate = function() {
-    // Do work when the extension is activated
-    console.log("ACTIVATED");
-    // Create the TreeView
-    treeView = new TreeView("todo", {
-        dataProvider: new ToDoDataProvider()
-    });
-    
-    treeView.onDidChangeSelection((selection) => {
-        // console.log("New selection: " + selection.map((e) => e.name));
-    });
-    
-    treeView.onDidExpandElement((element) => {
-        // console.log("Expanded: " + element.name);
-    });
-    
-    treeView.onDidCollapseElement((element) => {
-        // console.log("Collapsed: " + element.name);
-    });
-    
-    treeView.onDidChangeVisibility(() => {
-        // console.log("Visibility Changed");
-    });
-    
-    // TreeView implements the Disposable interface
-    nova.subscriptions.add(treeView);
+  // Do work when the extension is activated
+  
+  // Create the TreeView
+  treeView = new TreeView("todo", {
+    dataProvider: new ToDoDataProvider()
+  });
+  
+  treeView.onDidChangeSelection((selection) => {
+    // console.log("New selection: " + selection.map((e) => e.name));
+  });
+  
+  treeView.onDidExpandElement((element) => {
+    // console.log("Expanded: " + element.name);
+  });
+  
+  treeView.onDidCollapseElement((element) => {
+    // console.log("Collapsed: " + element.name);
+  });
+  
+  treeView.onDidChangeVisibility(() => {
+    // console.log("Visibility Changed");
+  });
+  
+  // TreeView implements the Disposable interface
+  nova.subscriptions.add(treeView);
 }
 
 exports.deactivate = function() {
-    // Clean up state before the extension is deactivated
+  // Clean up state before the extension is deactivated
 }
 
-
 nova.commands.register("todo.group", () => {
-    // Invoked when the "add" header button is clicked
-    console.log("Change grouping!");
+  // Invoked when the "add" header button is clicked
+  console.log("Change grouping!");
 });
 
 nova.commands.register("todo.refresh", () => {
-    // Invoked when the "remove" header button is clicked
-    let selection = treeView.selection;
-    console.log("Refresh!");
+  // Invoked when the "remove" header button is clicked
+  let selection = treeView.selection;
+  console.log("Refresh!");
 });
 
 nova.commands.register("todo.doubleClick", () => {
-    // Invoked when an item is double-clicked
-    let selection = treeView.selection;
-    console.log("DoubleClick: " + selection.map((e) => e.name));
+  // Invoked when an item is double-clicked
+  let selection = treeView.selection;
+  console.log("DoubleClick: " + selection.map((e) => e.name));
 });
 
 
