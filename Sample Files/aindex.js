8638f195be894664892194e234723f6aec6aa870
Jason Platts <plattsjm@icloud.com>
2020-12-03 15:10:18 -0500

    Updated sample file.

--- Sample Files/aindex.js
+++ Sample Files/aindex.js
@@ -1,1 +1,25 @@
-// NOTE: This
+// TODO: A message goes here.
+// FIXME: A message goes here.
+// BROKEN: A message goes here.
+// BUG: A message goes here.
+// DEBUG: A message goes here.
+// DEPRECATED: A message goes here.
+// EXAMPLE: A message goes here.
+// ERROR: A message goes here.
+// ERR: A message goes here.
+// FAIL: A message goes here.
+// FATAL: A message goes here.
+// HACK: A message goes here.
+// IDEA: A message goes here.
+// INFO: A message goes here.
+// NOTE: A message goes here.
+// OPTIMIZE: A message goes here.
+// QUESTION: A message goes here.
+// REFACTOR: A message goes here.
+// REMOVE: A message goes here.
+// REVIEW: A message goes here.
+// TASK: A message goes here.
+// TRACE: A message goes here.
+// UPDATE: A message goes here.
+// WARN: A message goes here.
+// WARNING: A message goes here.
