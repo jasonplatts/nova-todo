a89f76da5968abf591478e14b547a829055a0f6f
Jason Platts <plattsjm@icloud.com>
2020-12-23 18:35:04 -0500

    Adjusted test files.

--- Sample Files/index.php
+++ Sample Files/index.php
@@ -1,13 +1,14 @@
 <?php
   echo "This is a sample PHP file.";
-  //FIXME: Bad practice.
+  //FIXME: Avoid using this method if possible.
   
   echo "There is still something to do here.";
-  /* TODO: Must do this thing. TODO: This needs to be fixed. */ num = 1; //FIXME: Bad practice.
-  
+  /* TODO: Must add additional information to this field. */ num = 1; //FIXME: Bad practice, follow convention.
+  // IDEA: Improve the formatting.
   echo "An error should occur here.";
   //FIXME: Fix this error with the code.
-  //FIXME: DO THIS!
+  //ERROR: This number of incorrect in larger orders.
+  //NOTE: Talk to UI designers about modifying this file.
 ?>
 
 <h1>TODO APP</h1>
