8677b9c535f9e12712ba27d232ed5113789ae252
Jason Platts <plattsjm@icloud.com>
2021-05-24 16:36:36 -0400

    Added eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    'no-unused-vars': ['off'],
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
