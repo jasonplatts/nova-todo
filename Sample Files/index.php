efb9dee111fbca4d2d05437800f59b1f87f29a04
Jason Platts <48892071+jasonplatts@users.noreply.github.com>
2023-05-31 11:19:08 -0400

    Added testing of xxx tag.

--- Sample Files/index.php
+++ Sample Files/index.php
@@ -1,7 +1,7 @@
 <?php
   echo "This is a sample PHP file.";
   //FIXME: Avoid using this method if possible.
-  
+
   echo "There is still something to do here.";
   /* TODO: Must add additional information to this field. */ num = 1; //FIXME: Bad practice, follow convention.
   // IDEA: Improve the formatting.
@@ -9,6 +9,7 @@
   //FIXME: Fix this error with the code.
   //ERROR: This number of incorrect in larger orders.
   //NOTE: Talk to UI designers about modifying this file.
+  //XXX: Another test note.
 ?>
 
 <h1>TODO APP</h1>
